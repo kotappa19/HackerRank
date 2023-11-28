2454d0d091386a6127e4752e0a58e230b01884c6
Kotappa Y Gandudi <65644736+kotappa19@users.noreply.github.com>
2023-11-28 10:18:22 +0530

    saved

---
+++ C-C++/levelOrderTraversalBT.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
+        while (!q.empty()) {
+            Node* node = q.front();
+            q.pop();
+            if(node != NULL) {
+                cout << node->data << " ";
+                if(node->left != NULL) {
+                    q.push(node->left);
+                }
+                if(node->right != NULL) {
+                    q.push(node->right);
+                }
+            } else if(!q.empty()) {
+                q.push(NULL);
+            }
+        }
+
+    }
+
+}; //End of Solution
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.levelOrder(root);
+
+    return 0;
+}
